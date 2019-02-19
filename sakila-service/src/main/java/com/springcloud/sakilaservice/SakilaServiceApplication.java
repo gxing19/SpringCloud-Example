3cd71fce57cd13df489c08f475ec110f6a41c7b7
gxing <gxing19@163.com>
2019-02-20 00:11:27 +0800

    add service

---
+++ sakila-service/src/main/java/com/springcloud/sakilaservice/SakilaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.sakilaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakilaServiceApplication.class, args);
+    }
+
+}
