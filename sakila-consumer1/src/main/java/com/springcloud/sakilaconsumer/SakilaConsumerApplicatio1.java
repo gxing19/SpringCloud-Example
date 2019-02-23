610522f266f1a279d01be2afe0d7973dfb4d5b0c
gxing <gxing19@163.com>
2019-02-23 19:36:36 +0800

    change module

--- sakila-consumer1/src/main/java/com/springcloud/sakilaconsumer/SakilaConsumerApplicatio1.java
+++ sakila-consumer1/src/main/java/com/springcloud/sakilaconsumer/SakilaConsumerApplicatio1.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SakilaConsumerApplication {
+public class SakilaConsumerApplicatio1 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SakilaConsumerApplication.class, args);
+        SpringApplication.run(SakilaConsumerApplicatio1.class, args);
     }
 
 }
--- sakila-service1/src/main/java/com/springcloud/sakilaservice/SakilaServiceApplication1.java
+++ sakila-service1/src/main/java/com/springcloud/sakilaservice/SakilaServiceApplication1.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SakilaServiceApplication {
-
+public class SakilaServiceApplication1 {
+    
     public static void main(String[] args) {
-        SpringApplication.run(SakilaServiceApplication.class, args);
+        SpringApplication.run(SakilaServiceApplication1.class, args);
 //        new SpringApplicationBuilder(SakilaServiceApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 
