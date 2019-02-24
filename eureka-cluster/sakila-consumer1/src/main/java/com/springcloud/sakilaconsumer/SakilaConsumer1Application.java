baf38367069eb16bdcd3e06b5c5eaa4c8be0f639
gxing <gxing19@163.com>
2019-02-24 23:23:37 +0800

    add eureka cluster demo

---
+++ eureka-cluster/sakila-consumer1/src/main/java/com/springcloud/sakilaconsumer/SakilaConsumer1Application.java
@@ -0,0 +1,13 @@
+package com.springcloud.sakilaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaConsumer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakilaConsumer1Application.class, args);
+    }
+
+}
