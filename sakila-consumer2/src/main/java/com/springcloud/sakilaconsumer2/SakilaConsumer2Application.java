bcfb9ec641a40396e673a654a63debb634fe2aaf
gxing <gxing19@163.com>
2019-03-26 09:45:27 +0800

    创建文件

---
+++ sakila-consumer2/src/main/java/com/springcloud/sakilaconsumer2/SakilaConsumer2Application.java
@@ -0,0 +1,13 @@
+package com.springcloud.sakilaconsumer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaConsumer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakilaConsumer2Application.class, args);
+    }
+
+}
---
+++ sakila-service2/src/main/java/com/springcloud/sakilaservice2/SakilaService2Application.java
@@ -0,0 +1,13 @@
+package com.springcloud.sakilaservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakilaService2Application.class, args);
+    }
+
+}
