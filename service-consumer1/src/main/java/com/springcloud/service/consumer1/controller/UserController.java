18ca2797f105feb8549b41d90f64fcff7026f870
gxing <gxing19@163.com>
2019-04-04 18:55:57 +0800

    研究cookie

--- service-consumer1/src/main/java/com/springcloud/service/consumer1/controller/UserController.java
+++ service-consumer1/src/main/java/com/springcloud/service/consumer1/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.springcloud.service.consumer1.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springcloud.service.consumer1.service.UserInterface;
 import feign.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
 /**
@@ -24,9 +32,19 @@ public class UserController {
     private UserInterface userInterface;
 
     @GetMapping("/getUser/{age}")
-    public String getUser(@PathVariable Integer age){
-        HashMap<String,Integer> paramMap = new HashMap<>();
-        paramMap.put("age",age);
-        return userInterface.getUser(paramMap);
+    public String getUser(@PathVariable Integer age, HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        HashMap<String, Integer> paramMap = new HashMap<>();
+        paramMap.put("age", age);
+
+        String user = userInterface.getUser(paramMap);
+
+        Cookie cookie = new Cookie("user",URLEncoder.encode(user,"utf-8"));
+        cookie.setMaxAge(20);
+        response.addCookie(cookie);
+        response.setHeader("header", user);
+
+        System.out.println(JSON.toJSONString(request.getCookies()));
+
+        return user;
     }
 }
--- service-user/src/main/java/com/springcloud/service/user/controller/UserController.java
+++ service-user/src/main/java/com/springcloud/service/user/controller/UserController.java
@@ -1,11 +1,21 @@
 package com.springcloud.service.user.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.springcloud.service.user.entity.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 /**
  * @name: ApiController
@@ -21,9 +31,17 @@ public class UserController {
     private Integer serverPort;
 
     @GetMapping("/getUser")
-    public User getUser(Integer age){
+    public User getUser(Integer age, HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+
         User user = new User()
                 .setId(1001).setAge(age).setRealName("张三").setPort(serverPort);
+
+        String userStr = JSON.toJSONString(user);
+        Cookie cookie = new Cookie("userCookie", URLEncoder.encode(userStr, "utf-8"));
+        cookie.setMaxAge(10);
+        response.addCookie(cookie);
+        response.setHeader("userHeader", userStr);
+
         return user;
     }
 
