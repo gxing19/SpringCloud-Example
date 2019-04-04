6d9fb387d0a3768cc74b81b44536749a27dc95c0
gxing <gxing19@163.com>
2019-04-04 08:57:14 +0800

    注释

--- gateway-zuul/src/main/java/com/springcloud/gateway/config/ZuulConfig.java
+++ gateway-zuul/src/main/java/com/springcloud/gateway/config/ZuulConfig.java
@@ -1,15 +1,12 @@
 package com.springcloud.gateway.config;
 
-import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 /**
  * @name: ZuulConfig
  * @desc: TODO
  * @author: gxing
  * @date: 2019-04-03 15:27
  **/
+/*
 @Configuration
 public class ZuulConfig {
 
@@ -19,4 +16,4 @@ public class ZuulConfig {
                 "(?<name>^.+)-(?<version>v.+$)",
                 "${version}/${name}");
     }
-}
+}*/
