d36f9b9b3b75ffb6b6ea0fda1dd1433922cf0c48
gxing <gxing19@163.com>
2019-04-08 10:05:58 +0800

    自定义 zuul 配置

--- gateway-zuul/src/main/java/com/springcloud/gateway/config/ZuulConfig.java
+++ gateway-zuul/src/main/java/com/springcloud/gateway/config/ZuulConfig.java
@@ -1,13 +1,17 @@
 package com.springcloud.gateway.config;
 
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @name: ZuulConfig
  * @desc: TODO
  * @author: gxing
  * @date: 2019-04-03 15:27
  **/
-/*
-@Configuration
+
+/*@Configuration
 public class ZuulConfig {
 
     @Bean
