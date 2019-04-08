0781635f3a5880cf4a4e8ffcc73469be2bec2e35
gxing <gxing19@163.com>
2019-04-08 10:06:36 +0800

    测试

---
+++ gateway-zuul/src/test/java/com/springcloud/gateway/simple/Test1.java
@@ -0,0 +1,14 @@
+package com.springcloud.gateway.simple;
+
+
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    public void strLength() {
+        String str = "springboot-app-39-DB-V-flyway-liquibase";
+        System.out.println(str.length());
+    }
+
+}
