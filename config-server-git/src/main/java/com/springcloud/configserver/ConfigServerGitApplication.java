0992ac6515e9214cdff6aeaae065bf26109b81ab
gxing <gxing19@163.com>
2019-04-16 10:02:42 +0800

    修改项目结构

---
+++ config-server-git/src/main/java/com/springcloud/configserver/ConfigServerGitApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerGitApplication.class, args);
+    }
+
+}
