4eb5f22cc6ac5d6857fcbc86238c1a3fd06ca9d0
gxing <gxing19@163.com>
2019-04-17 23:10:37 +0800

    add retry

--- service-consumer1/src/main/java/com/springcloud/service/consumer1/controller/UserController.java
+++ service-consumer1/src/main/java/com/springcloud/service/consumer1/controller/UserController.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springcloud.service.consumer1.service.UserInterface;
 import feign.Param;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +29,7 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger logger = LogManager.getLogger(UserController.class);
 
     @Autowired(required = false)
     private UserInterface userInterface;
