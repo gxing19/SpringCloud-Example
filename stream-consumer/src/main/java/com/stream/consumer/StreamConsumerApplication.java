f113ebfba565aa11a7a047584b40fbf993d56d44
gxing <gxing19@163.com>
2019-05-23 18:01:41 +0800

    create spring cloud stream consumer and producer

---
+++ stream-consumer/src/main/java/com/stream/consumer/StreamConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.stream.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerApplication.class, args);
+    }
+
+}
---
+++ stream-producer/src/main/java/com/stream/producer/StreamProducerApplication.java
@@ -0,0 +1,13 @@
+package com.stream.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamProducerApplication.class, args);
+    }
+
+}
