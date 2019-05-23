ecfccb6b08a4e98b31c18809294f045e53573621
gxing <gxing19@163.com>
2019-05-23 18:01:54 +0800

    create spring cloud stream consumer and producer

---
+++ stream-producer/src/main/java/com/stream/producer/controller/ShopMallController.java
@@ -0,0 +1,26 @@
+package com.stream.producer.controller;
+
+import com.stream.producer.component.ShopMallProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @name: ShopMallController
+ * @desc: TODO
+ * @author: gxing
+ * @date: 2019-05-23 15:53
+ **/
+@RestController
+@RequestMapping("/shopmall")
+public class ShopMallController {
+
+    @Autowired
+    private ShopMallProducer shopMallProducer;
+
+    @RequestMapping("/order/{order}")
+    public void order(@PathVariable String order){
+        shopMallProducer.sendOrderInfo(order);
+    }
+}
---
+++ stream-producer/src/main/java/com/stream/producer/service/MsgService.java
@@ -0,0 +1,14 @@
+package com.stream.producer.service;
+
+/**
+ * @name: MsgService
+ * @desc: TODO
+ * @author: gxing
+ * @date: 2019-05-22 15:21
+ **/
+public interface MsgService {
+
+    void sendNBANews(String msg);
+
+    void sendMusicNews(String msg);
+}
